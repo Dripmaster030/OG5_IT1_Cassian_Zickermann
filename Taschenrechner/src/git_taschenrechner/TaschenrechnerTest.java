c097de02aaa6c4c4c7c5e8bc70163b665b1cb1af
Cassian Zickermann <user@192.168.34.95>
2026-09-03 10:36:53 +0200

    Taschenrechner Erste Version hochgeladen/Implementierung Aufgabe A2.5

--- Taschenrechner/src/git_taschenrechner/TaschenrechnerTest.java
+++ Taschenrechner/src/git_taschenrechner/TaschenrechnerTest.java
@@ -19,27 +19,44 @@ public class TaschenrechnerTest {
 		System.out.println("| Options:                 |");
 		System.out.println("|        1. Addieren       |");
 		System.out.println("|        2. Subtrahieren   |");
-		System.out.println("|        3. Dividieren     |");
-		System.out.println("|        4. Exit           |");
+		System.out.println("|        3. Multiplikation |");
+		System.out.println("|        4. Dividieren     |");
+		System.out.println("|        5. Exit           |");
 		System.out.println("============================");
 		System.out.print(" Select option: ");	
 		swValue = myScanner.next().charAt(0);
-
+		if (swValue == '1' || swValue == '2' ||swValue == '3' || swValue == '4' ) {
+		System.out.print("Geben sie die 1 Zahl ein: ");
+		int zahl1 = myScanner.nextInt();
+		System.out.print("Geben sie die 2 Zahl ein: ");
+		int zahl2 = myScanner.nextInt();
 		// Switch construct
 		switch (swValue) {
 		case '1':
-			System.out.println("4 + 5 = " + ts.add(4, 5));
+			System.out.println(zahl1 +" + "+ zahl2 +" = "+ ts.add(zahl1, zahl2));
 			break;
-			
-			
-		  //  add your code here
-		  
-		  
+		case '2': 
+			System.out.println(zahl1 +" + "+ zahl2 +" = "+ ts.sub(zahl1, zahl2));
+			break;
+		case '3':
+			System.out.println(zahl1 +" + "+ zahl2 +" = "+ ts.mul(zahl1, zahl2));
+			break;
+		case '4': 
+			System.out.println(zahl1 +" + "+ zahl2 +" = "+ ts.div(zahl1, zahl2));
+			break;
+		case '5': 
+			break;
+
+
+			//  add your code here
+
+
 		default:
 			System.out.println("Invalid selection");
 			break; // This break is not really necessary
 		}
-
+		}
+		System.out.println("System Beendet.");
 	}
 
 }
