8a06c1064ae6264296fd6fef03a514a69a85eea2
Cassian <user@192.168.34.192>
2026-09-17 10:33:02 +0200

    Die Klassen implementiert mit Attributen und Methoden und alle Fehler nebenbei behoben.

--- StarSim2099/src/de/oszimt/starsim2099/Pilot.java
+++ StarSim2099/src/de/oszimt/starsim2099/Pilot.java
@@ -7,9 +7,47 @@ package de.oszimt.starsim2099;
  * @version (a version number or a date)
  */
 public class Pilot {
-
-	// Attribute
+//Atribute 
+	private double posX;
+	private double posY;
+	private String grad;
+	private String name;
+	
+	
 	
 	// Methoden
+public Pilot() {}
 
+public double getPosX() {
+	return posX;
+}
+
+public void setPosX(double posX) {
+	this.posX = posX;
+}
+
+public double getPosY() {
+	return posY;
+}
+
+public void setPosY(double posY) {
+	this.posY = posY;
+}
+
+public String getGrad() {
+	return grad;
+}
+
+public void setGrad(String grad) {
+	this.grad = grad;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+	
 }
--- StarSim2099/src/de/oszimt/starsim2099/Raumschiff.java
+++ StarSim2099/src/de/oszimt/starsim2099/Raumschiff.java
@@ -9,9 +9,51 @@ package de.oszimt.starsim2099;
 public class Raumschiff {
 
 	// Attribute
+	private double posX;
+	private double posY;
+	private int maxLadekapazitaet;
+	private String typ;
+	private String antrieb;
+	private int winkel;
 	
 	// Methoden
-
+public Raumschiff() {}
+	public double getPosX() {
+	return posX;
+}
+public void setPosX(double posX) {
+	this.posX = posX;
+}
+public double getPosY() {
+	return posY;
+}
+public void setPosY(double posY) {
+	this.posY = posY;
+}
+public int getMaxLadekapazitaet() {
+	return maxLadekapazitaet;
+}
+public void setMaxLadekapazitaet(int ladekapazitaet) {
+	this.maxLadekapazitaet = ladekapazitaet;
+}
+public String getTyp() {
+	return typ;
+}
+public void setTyp(String typ) {
+	this.typ = typ;
+}
+public String getAntrieb() {
+	return antrieb;
+}
+public void setAntrieb(String antrieb) {
+	this.antrieb = antrieb;
+}
+public int getWinkel() {
+	return winkel;
+}
+public void setWinkel(int winkel) {
+	this.winkel = winkel;
+}
 	// Darstellung
 	public static char[][] getDarstellung() {
 		char[][] raumschiffShape = { 
